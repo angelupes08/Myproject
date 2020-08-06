e678362cb3a38cf830408cea8ffe405efee3b68e
ANGEL <ANGEL@DESKTOP-VM3P6KA.domain.name>
2020-08-06 09:47:11 +0530

    added diff method

--- EmployeeManagementSystem/src/com/lti/Calculator.java
+++ EmployeeManagementSystem/src/com/lti/Calculator.java
@@ -6,6 +6,9 @@ public class Calculator {
 	public static void add(int a,int b){
 		System.out.println("Sum= "+(a+b));
 	}
+	public static void diff(int a,int b){
+		System.out.println("Sum= "+(a-b));
+	}
 	public static void main(String[] args) {
 		add(10,30);
 
